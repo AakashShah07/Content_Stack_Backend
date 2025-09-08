dae301ef4550c5cf256413e02177c77d2036a7ce
AakashShah07 <angelayusender@gmail.com>
2025-09-09 01:50:14 +0530

    MongoDB SetUp

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const connectDB = require("./Database/db")
+const Entry  =  require("./Model/entryModel")
 const express = require("express");
 const app = express();
 
 require("dotenv").config();
 const contentstack = require("contentstack");
+const { connect } = require("mongoose");
+
+connectDB();
 
 const Stack = contentstack.Stack({
   api_key: process.env.CONTENTSTACK_API_KEY,
@@ -27,9 +32,25 @@ app.get("/", async (req, res) => {
     })
     const result = await query.find();
 
-    result[0].forEach((entry) => {
-      console.log("Title:", entry.title);
-    });
+    result[0].forEach(async (entry) => {
+     const saved =  await Entry.findOneAndUpdate(
+        { contentstackId: entry.uid }, // ensure correct UID
+        { title: entry.title, contentstackId: entry.uid },
+        { upsert: true, new: true })
+                  console.log("✅ Saved in MongoDB:", saved);
+
+    })
+    // for (const entry of entries) {
+    //   console.log("📌 Entry from Contentstack:", entry);
+
+    //   const saved = await Entry.findOneAndUpdate(
+    //     { contentstackId: entry.uid }, // ensure correct UID
+    //     { title: entry.title, contentstackId: entry.uid },
+    //     { upsert: true, new: true })
+      
+    // }
+          console.log("✅ Saved in MongoDB:");
+
   } catch (err) {
     console.error("Error fetching entries:", err);
   }
